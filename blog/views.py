9853c7e54b34de86dd0608f439b8fe52a18b13a9
O'nesmo <kasongoonesmo33@gmail.com>
2020-10-11 04:07:31 +0530

    added a blog url config, added a dummy views to blog, amd configured links to create blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+
+def create_blog_view(request):
+    """ view for creating a blog """
+
+    return render(request, 'blog/create_blog.html', {})
