bc61e47337f45bafd727d53512aadf65e2bd445a
O'nesmo <kasongoonesmo33@gmail.com>
2020-10-11 21:30:20 +0530

    displayed blogs on home page

--- personal/views.py
+++ personal/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from account.models import Account
+from operator import attrgetter
+from blog.models import BlogPost
 # Create your views here.
 
 def home_screen_view(request):
-
-    users = Account.objects.all()
-    context = {'users': users}
+    
+    context = {}
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
     return render(request, 'personal/home.html', context)
